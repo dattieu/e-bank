c3749cc5510c795fc54301e94ed19f2e60539ae2
dattieu <dat.tieu@aleph-labs.com>
2023-05-21 13:47:01 +0700

    config sonar

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,11 @@
+-- seed data --
+
+-- table customers
 INSERT INTO customers (id, name, phone, address) VALUES (1, 'John Doe', '555-356-0985', 'England');
 INSERT INTO customers (id, name, phone, address) VALUES (2, 'Dat Tieu', '0917106789', 'Viet Nam');
 INSERT INTO customers (id, name, phone, address) VALUES (3, 'Sheng Lu', '487-098-111', 'Hong Kong');
+
+-- table products
+INSERT INTO products (id, product_id, product_version, product_name) VALUES (1, 'csa-123', '1.0.0', 'casa');
+INSERT INTO products (id, product_id, product_version, product_name) VALUES (2, 'crd-123', '1.0.0', 'credit');
+INSERT INTO products (id, product_id, product_version, product_name) VALUES (3, 'ln-123', '1.0.0', 'loan');
