96d54ac486875fdcba23b90f7292cb47f7c8ff93
dattieu <dat.tieu@aleph-labs.com>
2023-05-21 15:11:00 +0700

    add sonar plugin

--- src/main/java/com/dtu/banking/core/config/exception/ExceptionHandler.java
+++ src/main/java/com/dtu/banking/core/config/exception/ExceptionHandler.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.zalando.problem.ProblemModule;
+import org.zalando.problem.jackson.ProblemModule;
 import org.zalando.problem.spring.web.advice.security.SecurityAdviceTrait;
-import org.zalando.problem.validation.ConstraintViolationProblemModule;
+import org.zalando.problem.violations.ConstraintViolationProblemModule;
 
 @ControllerAdvice
 @EnableAutoConfiguration(exclude = ErrorMvcAutoConfiguration.class)
